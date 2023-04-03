e8983b677eb35b1371b00fc5b574a4a7973c84a1
Leandro Glassman <leandroglassman1@gmail.com>
2023-04-03 00:13:47 -0300

    removing outdated lines

--- src/main/java/CustomFramework/utils/DriverManager.java
+++ src/main/java/CustomFramework/utils/DriverManager.java
@@ -1,36 +1,29 @@
 package CustomFramework.utils;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
 
 public class DriverManager {
     private static WebDriver driver;
 
     public static WebDriver getDriver(){
         if(driver == null) {
-            // set properties
-            // this line download the necessary webdriver
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-//            WebDriverManager.chromedriver().setup();
-//              WebDriverManager.firefoxdriver().setup();
+            //Set properties
+            //Line to download the driver manually
+            //System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+            WebDriverManager.chromedriver().setup();
 
             // configure chrome options here
             ChromeOptions options = new ChromeOptions();
-//              FirefoxOptions options = new FirefoxOptions();
 
             options.addArguments("--start-maximized");
             options.addArguments("--disable-notifications");
-            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--remote-allow-origins=*");//necessary for the driver's error
 
             // initialize the driver
             driver = new ChromeDriver(options);
-//              driver = new FirefoxDriver(options);
         }
         return driver;
     }
